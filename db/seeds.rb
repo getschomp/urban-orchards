0ee85501bca67f701bed7e4cf1b65bf3c21b0e23
Allison <allisonmariebrowne@gmail.com>
2015-03-26 01:36:20 -0400

    configured httparty

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+response = HTTParty.get("https://data.cityofboston.gov/resource/c7cz-29ak.json")
